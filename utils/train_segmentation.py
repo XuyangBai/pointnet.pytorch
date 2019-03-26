fccdb7951378c60710fa8e00b86aeab5a192f8ec
xuyangbai <653823597@qq.com>
2019-03-26 19:59:44 +0800

    update verbose scheme for segmentation

--- utils/train_segmentation.py
+++ utils/train_segmentation.py
@@ -2,16 +2,17 @@ from __future__ import print_function
 import argparse
 import os
 import random
+import time
 import torch
 import torch.nn.parallel
 import torch.optim as optim
+from torch.autograd import Variable
 import torch.utils.data
 from pointnet.dataset import ShapeNetDataset
 from pointnet.model import PointNetDenseCls, feature_transform_reguliarzer
 import torch.nn.functional as F
 from tqdm import tqdm
 import numpy as np
-
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -64,8 +65,6 @@ try:
 except OSError:
     pass
 
-blue = lambda x: '\033[94m' + x + '\033[0m'
-
 classifier = PointNetDenseCls(k=num_classes, feature_transform=opt.feature_transform)
 
 if opt.model != '':
@@ -77,8 +76,28 @@ classifier.cuda()
 
 num_batch = len(dataset) / opt.batchSize
 
+
+def get_iou(pred_np, target_np):
+    shape_ious = []
+    for shape_idx in range(target_np.shape[0]):
+        parts = range(num_classes)  # np.unique(target_np[shape_idx])
+        part_ious = []
+        for part in parts:
+            I = np.sum(np.logical_and(pred_np[shape_idx] == part, target_np[shape_idx] == part))
+            U = np.sum(np.logical_or(pred_np[shape_idx] == part, target_np[shape_idx] == part))
+            if U == 0:
+                iou = 1  # If the union of groundtruth and prediction points is empty, then count part IoU as 1
+            else:
+                iou = I / float(U)
+            part_ious.append(iou)
+        shape_ious.append(np.mean(part_ious))
+    return np.mean(shape_ious)
+
+
 for epoch in range(opt.nepoch):
     scheduler.step()
+    loss_buf = []
+    epoch_start_time = time.time()
     for i, data in enumerate(dataloader, 0):
         points, target = data
         points = points.transpose(2, 1)
@@ -88,56 +107,48 @@ for epoch in range(opt.nepoch):
         pred, trans, trans_feat = classifier(points)
         pred = pred.view(-1, num_classes)
         target = target.view(-1, 1)[:, 0] - 1
-        #print(pred.size(), target.size())
+        # print(pred.size(), target.size())
         loss = F.nll_loss(pred, target)
+        loss_buf.append(loss.detach().cpu().numpy())
         if opt.feature_transform:
             loss += feature_transform_reguliarzer(trans_feat) * 0.001
         loss.backward()
         optimizer.step()
         pred_choice = pred.data.max(1)[1]
         correct = pred_choice.eq(target.data).cpu().sum()
-        print('[%d: %d/%d] train loss: %f accuracy: %f' % (epoch, i, num_batch, loss.item(), correct.item()/float(opt.batchSize * 2500)))
+        # print('[%d: %d/%d] train loss: %f accuracy: %f' % (epoch, i, num_batch, loss.item(), correct.item()/float(opt.batchSize * 2500)))
 
-        if i % 10 == 0:
-            j, data = next(enumerate(testdataloader, 0))
+    # finish one epoch
+    print('Epoch %d: Train loss: %.2f, Time: %.2f s' % (epoch, np.mean(loss_buf), time.time() - epoch_start_time))
+    # every ten epoch: save model & evaluate
+
+    if (1 + epoch) % 10 == 0:
+        torch.save(classifier.state_dict(), '%s/seg_model_%s_%d.pth' % (opt.outf, opt.class_choice, epoch + 1))
+        # evaluate on test set
+        loss_buf = []
+        correct_buf = []
+        iou_buf = []
+        for i, data in enumerate(testdataloader, 0):
             points, target = data
+            points, target = Variable(points), Variable(target[:, 0])
             points = points.transpose(2, 1)
             points, target = points.cuda(), target.cuda()
-            classifier = classifier.eval()
             pred, _, _ = classifier(points)
             pred = pred.view(-1, num_classes)
             target = target.view(-1, 1)[:, 0] - 1
             loss = F.nll_loss(pred, target)
             pred_choice = pred.data.max(1)[1]
-            correct = pred_choice.eq(target.data).cpu().sum()
-            print('[%d: %d/%d] %s loss: %f accuracy: %f' % (epoch, i, num_batch, blue('test'), loss.item(), correct.item()/float(opt.batchSize * 2500)))
+            correct = pred_choice.eq(target.data.cpu().sum())
 
-    torch.save(classifier.state_dict(), '%s/seg_model_%s_%d.pth' % (opt.outf, opt.class_choice, epoch))
+            # calculate the mIOU
+            pred_np = pred_choice.cpu().data.numpy()
+            target_np = target.cpu().data.numpy()
+            shape_iou = get_iou(pred_np, target_np)
 
-## benchmark mIOU
-shape_ious = []
-for i,data in tqdm(enumerate(testdataloader, 0)):
-    points, target = data
-    points = points.transpose(2, 1)
-    points, target = points.cuda(), target.cuda()
-    classifier = classifier.eval()
-    pred, _, _ = classifier(points)
-    pred_choice = pred.data.max(2)[1]
-
-    pred_np = pred_choice.cpu().data.numpy()
-    target_np = target.cpu().data.numpy() - 1
-
-    for shape_idx in range(target_np.shape[0]):
-        parts = range(num_classes)#np.unique(target_np[shape_idx])
-        part_ious = []
-        for part in parts:
-            I = np.sum(np.logical_and(pred_np[shape_idx] == part, target_np[shape_idx] == part))
-            U = np.sum(np.logical_or(pred_np[shape_idx] == part, target_np[shape_idx] == part))
-            if U == 0:
-                iou = 1 #If the union of groundtruth and prediction points is empty, then count part IoU as 1
-            else:
-                iou = I / float(U)
-            part_ious.append(iou)
-        shape_ious.append(np.mean(part_ious))
-
-print("mIOU for class {}: {}".format(opt.class_choice, np.mean(shape_ious)))
+            iou_buf.append(shape_iou)
+            loss_buf.append(loss.data.item())
+            correct_buf.append(correct.item())
+        # print('i:%d  loss: %f accuracy: %f' % (i, loss.data.item(), correct / float(32)))
+        acc = np.sum(correct_buf) * 100 / test_dataset.__len__() / opt.batchSize / ShapeNetDataset.npoints
+        print("Epoch %d: Test loss: %.4f, Accuracy: %.2f\%, IOU: %.2f\%" % (
+            epoch, np.mean(loss_buf), acc, np.mean(iou_buf) * 100.0))
